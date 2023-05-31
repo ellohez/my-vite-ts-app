adca384564f9da5a2e3c49481767bbcd35b49872
ellohez <helenpjobs@gmail.com>
2023-05-31 15:06:32 +0100

    State and handlers added for input field

--- src/components/BasicForm/index.tsx
+++ src/components/BasicForm/index.tsx
@@ -1,5 +1,4 @@
-import React, { FC } from 'react';
-
+import React, { FocusEvent, FC, useState, ChangeEvent } from 'react';
 interface TitleProps {
   title: string; // Required 
   subtitle?: string; // optional
@@ -8,6 +7,22 @@ interface TitleProps {
 
 // FC (Functional Component) interface always adds the 'children' prop 
 const BasicForm: FC<TitleProps> = ({ title, subtitle, children }) => {
+
+  // 
+  const [userInput, setUserInput] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [isBlur, setIsBlur] = useState(false);
+
+  const blurHandler = (e: FocusEvent<HTMLInputElement>) => {
+    setUserInput(e.target.value);
+    setIsBlur(true);
+    // For now - consider input valid if it contains '@'
+    setIsValid(userInput.includes('@'));
+  }
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      setUserInput(e.target.value);
+  }
     
   return (
       <div>
@@ -15,14 +30,23 @@ const BasicForm: FC<TitleProps> = ({ title, subtitle, children }) => {
         <h3>{subtitle}</h3>
         <div>{children}</div>
 
-        {/* TODO - checkout aria-placeholder, would descriptive label be preferable? */}
-        <label htmlFor='input1'>Input1:</label>
-        <input
-          name='input1'
-          id='input1'
-          placeholder='your input'
-          aria-placeholder='your input'
-        />
+      <form>
+          {/* TODO - checkout aria-placeholder, would descriptive label be preferable? */}
+          <label htmlFor='input1' id='inputLabel'>Input1:</label>
+          <input
+            name='input1'
+            id='input1'
+            placeholder='your input here'
+            aria-placeholder='your input here'
+            aria-labelledby='inputLabel'
+            aria-required='true'
+            value={userInput}
+            onBlur={blurHandler}
+            onChange={changeHandler}
+          />
+          {isBlur && !isValid && <p className="error">The name you entered is not valid</p>}
+          {isBlur && isValid && <p className="success">The name you entered looks good</p>}
+      </form>
       </div>
   )
 }
