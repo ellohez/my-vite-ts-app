795da32e7763fa4bf4bab78518b227508fe18b70
ellohez <helenpjobs@gmail.com>
2023-05-31 17:38:16 +0100

    reset css styles and add local

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 {/* TODO - reset css styles and create own */}
-// import './reset.css'
+import './reset.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
