ecf6408525e238044a338767b861b578db78a719
ellohez <helenpjobs@gmail.com>
2023-06-01 18:09:35 +0100

    Form elements -> LoginDetails + logic for pages

--- src/components/SignupForm/index.tsx
+++ src/components/SignupForm/index.tsx
@@ -1,64 +1,61 @@
-import React, { FocusEvent, FC, useState, ChangeEvent } from 'react';
+import { useState } from 'react';
+import LoginDetails from '../LoginDetails';
 import './styles.css'
 
+// FC (Functional Component) interface does not include 'children'
+// If needed, add to interface as optional (using '?')
+const SignupForm = (): JSX.Element => {
 
-// FC (Functional Component) interface always adds the 'children' prop 
-const SignupForm: FC = () => {
+  const formTitles: Array<string> = ['Sign Up',
+                      'Login Details', 
+                      'Personal Information',
+                      'Address'];
+// TODO: Check TS - using object as state with initial null 
+  const [page, setPage] = useState(0);
+  const [formData, setFormData] = useState({
+                    email: '',
+                    password: '',
+                    firstName: '',
+                    surname: '',
+                    birthDate: null,
+                    address: '',
+  })
 
-  // 
-  const [userInput, setUserInput] = useState('');
-  const [isValid, setIsValid] = useState(false);
-  const [isBlur, setIsBlur] = useState(false);
-
-  const blurHandler = (e: FocusEvent<HTMLInputElement>) => {
-    setUserInput(e.target.value);
-    setIsBlur(true);
-    // For now - consider input valid if it contains '@'
-    setIsValid(userInput.includes('@'));
-  }
-
-  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-      setUserInput(e.target.value);
-  }
+  const PageDisplay = () => {
+      if(page === 1) {
+        return <LoginDetails />
+      }
+  };
     
   return (
-      <>
-      <div className='grid'>
-          {/* TODO - checkout aria-placeholder, would descriptive label be preferable? */}
-          <label 
-            className='input-label'
-            htmlFor='input1' 
-            id='inputLabel1'>Input1:</label>
-          <input
-            className='bold-input'
-            name='input1'
-            id='input1'
-            placeholder='your input here'
-            aria-placeholder='your input here'
-            aria-labelledby='inputLabel1'
-            aria-required='true'
-            value={userInput}
-            onBlur={blurHandler}
-            onChange={changeHandler}
-          />
-          {isBlur && !isValid && <p className="error">The name you entered is not valid</p>}
-          {isBlur && isValid && <p className="success">The name you entered looks good</p>}
-
-          <label htmlFor='input2' id='inputLabel2'>Input2:</label>
-          <input
-            className='bold-input'
-            name='input2'
-            id='input2'
-            placeholder='more input here'
-            aria-placeholder='more input here'
-            aria-labelledby='inputLabel2'
-            aria-required='true'
-            // value={userInput}
-            // onBlur={blurHandler}
-            // onChange={changeHandler}
-          />
+    <div className='form'>
+      <div className='bread-crumb'>
+        <div className='form-container'>
+            <div className='header'>
+              {/* Display the relevant title for the current page */}
+              <h1>{formTitles[page]}</h1>
+            </div>
+            {/* Load the relevant component for the page number */}
+            <div className='body'>{PageDisplay()}</div>
+            <div className='footer'>
+                <button
+                  disabled={page == 0}
+                  onClick={() => {
+                    setPage((currentPg) => currentPg - 1);
+                  }}>
+                  Previous
+                </button>
+                <button
+                  disabled={page == formTitles.length - 1}
+                  onClick={() => {
+                    setPage((currentPg) => currentPg + 1);
+                  }}>
+                  Next
+                </button>
+            </div>
+        </div>
       </div>
-      </>
+    </div>
   )
 }
 
