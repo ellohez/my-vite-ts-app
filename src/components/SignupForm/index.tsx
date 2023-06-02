c0541be356a714a24110d2b97d4e2f1d5fcb3d2e
ellohez <helenpjobs@gmail.com>
2023-06-02 17:59:09 +0100

    Breadcrumb display function created

--- src/components/SignupForm/index.tsx
+++ src/components/SignupForm/index.tsx
@@ -7,36 +7,61 @@ import './styles.css'
 const SignupForm = (): JSX.Element => {
 
   const formTitles: Array<string> = ['Sign Up',
-                      'Login Details', 
-                      'Personal Information',
-                      'Address'];
-// TODO: Check TS - using object as state with initial null 
+    'Login Details',
+    'Personal Information',
+    'Address'];
+
+  // TODO: Check TS - using object as state with initial null
   const [page, setPage] = useState(0);
+
   // TODO: TS type for formData
   const [formData, setFormData] = useState({
-                    email: '',
-                    password: '',
-                    firstName: '',
-                    surname: '',
-                    birthDate: null,
-                    address: '',
+    email: '',
+    password: '',
+    firstName: '',
+    surname: '',
+    birthDate: null,
+    address: '',
   })
 
   const PageDisplay = () => {
-      if(page === 1) {
-        return <LoginDetails />
-      }
+    if (page === 1) {
+      return <LoginDetails />
+    }
   };
-    
+
+  const BreadcrumbDisplay = () => {
+    return (
+      // TODO: Change this to a standard for loop 
+      // so we can calculate the steps remaining.
+      // or can we stop map when we get to the current stage?
+      // see - https://www.telerik.com/blogs/beginners-guide-loops-in-react-jsx 
+      <div className='breadcrumb-trail'>
+        {formTitles.map((title, index) => (
+          <div className='breadcrumb-item'>
+            <p>
+              Step {index + 1}
+            </p>
+            <p>
+              {title} &gt;
+            </p>
+          </div>
+        ))}
+        <div className='breadcrumb-item'>
+          <p>Steps remaining</p>
+          <p>TODO</p>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
       <div>
         <h1>Let's get you signed up!</h1>
       </div>
-      {/* Breadcrumb navigation */}
-      <div className='bread-crumb'>
-        {formTitles.join(' > ')}
-      </div>      
+      {/* Draw breadcrumb trail, showing where the user is up to */}
+      {BreadcrumbDisplay()}
       <div className='form-container'>
         {/* TODO: ?? Do we need a form element? */}
         {/* Load the relevant component for the page number */}
@@ -46,21 +71,21 @@ const SignupForm = (): JSX.Element => {
         </div>
         {PageDisplay()}
         <div className='row'>
-                <button
-                  disabled={page == 0}
-                  onClick={() => {
-                    setPage((currentPg) => currentPg - 1);
-                  }}>
-                  Previous
-                </button>
-                <button
-                  disabled={page == formTitles.length - 1}
-                  onClick={() => {
-                    setPage((currentPg) => currentPg + 1);
-                  }}>
-                  Next
-                </button>
-          </div>
+          <button
+            disabled={page == 0}
+            onClick={() => {
+              setPage((currentPg) => currentPg - 1);
+            }}>
+            Previous
+          </button>
+          <button
+            disabled={page == formTitles.length - 1}
+            onClick={() => {
+              setPage((currentPg) => currentPg + 1);
+            }}>
+            Next
+          </button>
+        </div>
       </div>
     </>
   )
