d850f430251305a3cedf25bac024891968ff5b57
ellohez <helenpjobs@gmail.com>
2023-06-06 15:19:03 +0100

    Moved import of reset.css to index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-{/* TODO - reset css styles and create own */}
-import './reset.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
